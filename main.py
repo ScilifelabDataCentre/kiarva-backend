45a7d90681ff3dab1c6df529957dbf7ee62ddc5f
SevLG <sebastian.gunnari.lindbom@scilifelab.se>
2024-05-27 20:51:08 +0200

    Some more initial work on backend, can write out fasta files of sequences separated by 'IGHV', 'IGHD', 'IGHJ', started setting up the model

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
-import os
 import pandas as pd
+from constants import ROOT_DIR
 
-def df_to_fasta(df, desc_col, seq_col):
-    df = df[[desc_col, seq_col]].drop_duplicates()
+def df_to_fasta(df, db_name_col, seq_col, seq_type):
+    df = df[df[db_name_col].str.startswith(seq_type)]
+    df = df[[db_name_col, seq_col]].drop_duplicates()
     fasta_out = ''
     for index, row in df.iterrows():
-        fasta_out += '>' + row[desc_col] + '\n' +row[seq_col] + '\n'
+        fasta_out += '>' + row[db_name_col] + '\n' +row[seq_col] + '\n'
     return fasta_out
+
+def write_fastas(df, output_dir):
+    seq_types = ['IGHV', 'IGHD', 'IGHJ']
+    for seq_type in seq_types: 
+        f = open(output_dir + seq_type + '.fasta', 'w')
+        f.write(df_to_fasta(df, 'db_name', 'sequence', seq_type))
+        f.close()
 
 
 def main():
+    data_in_dir = ROOT_DIR + "/data/in/"
+    data_out_dir = ROOT_DIR + "/data/out/"
+
     file_name = '1KGP_ImmuneDiscover_IGHV1-2.tsv'
-    file_dir = os.path.dirname(os.path.realpath('./data/'+file_name))
-    df = pd.read_csv(file_dir + '/' + file_name,sep='\t')
+
+    df = pd.read_csv(data_in_dir + file_name,sep='\t')
+
     print(df.dtypes)
-    pd.set_option('display.max_colwidth', None)
-    # print(df[['db_name', 'sequence']].drop_duplicates())
-    print(df_to_fasta(df, 'db_name', 'sequence'))
+    print(df[df.columns[:5]].head())
+    print()
+    print(df[df.columns[5:10]].head())
+    print()
+    print(df[df.columns[10:15]].head())
+    # write_fastas(df, data_out_dir)
 
 
 main()
