0bbe165823bbe68ed881e364cd80902b994527b1
SevLG <sebastian.gunnari.lindbom@scilifelab.se>
2024-07-02 19:27:24 +0200

    Discovered bug that crashed script when data was missing for an allele, reverting back to old version but with some improvements

--- utils/calculate_allele_frequencies.py
+++ utils/calculate_allele_frequencies.py
@@ -1,35 +1,31 @@
-from sqlalchemy import func
-from sqlalchemy.orm import aliased
+from collections import Counter, OrderedDict
 from models.immunediscoverdata import ImmuneDiscoverDataModel
 
 # calculate the frequency that an allele appears in a population, alt a superpopulation
 def calculate_allele_frequencies(allele_name, population_type):
-        distinct_cases = ImmuneDiscoverDataModel.query.with_entities(
+        cases = ImmuneDiscoverDataModel.query.with_entities(
+            ImmuneDiscoverDataModel.case,
+            getattr(ImmuneDiscoverDataModel, population_type)
+            ).distinct().all()
+        cases_with_allele = ImmuneDiscoverDataModel.query.with_entities(
             ImmuneDiscoverDataModel.case,
             getattr(ImmuneDiscoverDataModel, population_type),
-            ).distinct().subquery()
-        
-        aliased_distinct_cases = aliased(ImmuneDiscoverDataModel, distinct_cases)
+            ImmuneDiscoverDataModel.db_name
+            ).where(ImmuneDiscoverDataModel.db_name == allele_name).distinct().all()
 
-        pop_count = aliased_distinct_cases.query.with_entities(
-            getattr(aliased_distinct_cases, population_type),
-            aliased_distinct_cases.case,
-            func.count(getattr(aliased_distinct_cases, population_type))
-            ).group_by(getattr(aliased_distinct_cases, population_type)).all()
-        
-        pop_count_with_allele = aliased_distinct_cases.query.with_entities(
-            getattr(aliased_distinct_cases, population_type),
-            aliased_distinct_cases.case,
-            ImmuneDiscoverDataModel.db_name,
-            func.count(getattr(aliased_distinct_cases, population_type))
-            ).join(ImmuneDiscoverDataModel, aliased_distinct_cases.case == ImmuneDiscoverDataModel.case).where(ImmuneDiscoverDataModel.db_name == allele_name).group_by(getattr(aliased_distinct_cases, population_type)).all()
+        populations = [col[1] for col in cases]
+        populations_with_allele = [col[1] for col in cases_with_allele]
+        pop_count = Counter(populations)
+        pop_with_allele_count = Counter(populations_with_allele)
+
+        pop_count = OrderedDict(sorted(pop_count.items()))
 
         data_out = []
-        for i in range(len(pop_count)):
+        for pop in pop_count:
             data_out.append({
-                'population': pop_count[i][0],
-                'n': pop_count_with_allele[i][3],
-                'frequency': pop_count_with_allele[i][3]/pop_count[i][2]
+                'population': pop,
+                'n': pop_with_allele_count[pop],
+                'frequency': pop_with_allele_count[pop]/pop_count[pop]
             })
         
         return data_out
